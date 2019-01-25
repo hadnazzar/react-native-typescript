c5378080672dd1aa658a141598260dc890775205
Melih <melih193@gmail.com>
2019-01-25 01:05:21 +0100

    Sentry added

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,12 +20,16 @@ const instructions = Platform.select({
 
 interface Props {}
 export default class App extends Component<Props> {
+  onPress = () =>{
+    throw new Error("test1")
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="press me for error" onPress={this.onPress}></Button>
       </View>
     );
   }
--- index.js
+++ index.js
@@ -7,4 +7,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import { Sentry } from 'react-native-sentry';
+Sentry.config('https://31fa165832a44cfab2642d9c2a6ed1ce@sentry.io/1379190').install();
+
 AppRegistry.registerComponent(appName, () => App);
